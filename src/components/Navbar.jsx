dbb33f518420d372a4d7abf47035c6de1a4523c3
aydinhuseynov <aydinnhuseynli@gmail.com>
2025-03-18 23:46:38 +0400

    implement navbar component with styled-components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,114 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Bio } from "../data/constants";
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
 
 const NavbarContainer = styled.div`
-background-color: ${({theme}) => theme.bg };
-height:80px;
-display:flex;
-align-items:center;
-justify-content:center;
-font-size:1rem;
-position:sticky;
-top:0;
-z-index:99;
+  width: 100%;
+  max-width: 1200px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
 `;
-const NavLogo = styled(Link)``;
+const NavLogo = styled(Link)`
+  width: 80%;
+  padding: 0 6px;
+  font-weight: 500;
+  font-size: 18px;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const GithubButton = styled.a`
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  cursor: pointer;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
 
 export const Navbar = () => {
   return (
-    <NavbarContainer>
-        <NavLogo to="/">
-        <a style={{
-            color:"white"
-        }}>AH</a>
-        </NavLogo>
-    </NavbarContainer>
-  )
-}
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">AydinHuseynov</NavLogo>
+
+        <NavItems>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Project">Project</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+        </NavItems>
+
+        <ButtonContainer>
+          <GithubButton href={Bio.github} target="_blank">
+            Github Profile
+          </GithubButton>
+        </ButtonContainer>
+      </NavbarContainer>
+    </Nav>
+  );
+};
